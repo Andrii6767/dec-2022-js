e2b7e78be75b87c236a8474251445d23b072c91e
Andrii6767 <tsaiderandriy@gmail.com>
2023-02-13 23:14:18 +0200

    add fail

---
+++ lessons/lesson1/name.js
@@ -0,0 +1,127 @@
+// console.log(33); //вивід інформації у консоль розробника
+// document.write('hello'); // вивід інформації у браузерне вікно
+// let strText='Hello';// type string 'hello' "hello" `hello`
+// console.log(strText);
+//
+// const PI=3
+// console.log(PI);
+//
+// strText='world';
+// console.log(strText);
+
+// {
+    // let a=1;
+    // var b=2;
+    // console.log(a);
+//     // console.log(b);
+// }
+// console.log(a);
+// console.log(b);
+
+// let str='hello';
+// console.log(typeof str);
+//
+// let num=10;
+// console.log(typeof num);
+//
+// let bool=true;
+// console.log(typeof bool)
+//
+// let str2='false';
+// console.log(typeof str);
+
+// let str='Hello';
+
+// let str2='World';
+//конкадинація  - в рядку все буде писатися разом
+// console.log(str + str2);
+// для того аби не було консадинації пишемо:
+// let str3='pi';
+// console.log(str + ' qqq' +str2 + ' ' + ' ' + str3);
+//для того щоби ці всі + ' ' + стільки не ставив пишемо: console.log(`${str} ${str2} ${str3}`);
+
+// console.log(str + ' ' +str2);
+
+// console.log(`${str} brave new ${str2} ${str3}`);
+
+// let num = 10;
+// console.log(num);
+//
+// let num1 = 2;
+// console.log(num1);
+
+// console.log(num + num1);
+// console.log(num - num1);
+// console.log(num * num1);
+// console.log(num / num1);
+// console.log(num % num1);
+// 0.1+0.2.log=0.30000000000000004
+// console.log(0.1 + 0.2);  //0.30000000000000004
+
+// let big=BigInt(1234567);
+// console.log(big); //1234567n
+
+// let a=true;
+// // let b=false;
+// // console.log(typeof a);
+// // console.log(typeof b);
+// //
+// // let res =5 > 7; //false , true;
+// // console.log(res);
+// //
+// //
+// // console.log(10 === 10);
+// // console.log(10 !== 10);
+// // console.log(10 !== '10');
+// // console.log(10 == '10');
+// // console.log(10 === '10');
+
+// console.log('hello' === 'hello');
+// console.log('hell' <'hello');
+
+// console.log(typeof (22 +''));
+// // тобто 22 при +'' буде перетворюватися у стрінгу;
+//
+// console.log(100 + 200 + 'hello');
+//
+// console.log('hello' + 100 + 200);
+//
+// console.log(typeof '12');
+//
+// console.log(typeof +'12');
+//
+// console.log( +'12djdkjdke');
+//
+// console.log(parseInt('12kdsjfkl')); //виведе 12, вона відсікає все те що не є числом
+//
+// console.log(parseFloat('12.45')); // перетворювати в тим намбер parseFloat. parseInt.
+
+// console.log(!!10);
+// console.log(!!-10);
+// console.log(!!'10');
+// console.log(!!10);
+// console.log(!!-10);
+// console.log(!!'-10');
+// console.log(!![]);
+// console.log(!!{});
+//
+// console.log(!!0);
+// console.log(!!'');
+// console.log(!!NaN);
+// console.log(!!null);
+// console.log(!!undefined);
+// console.log(!!-0);
+
+// let obj={
+//     name: 'Vasya',
+//     age: 33
+// }
+// console.log(obj);
+//
+// let arr=[1,2,3,4,obj];
+// console.log(arr);
+//
+// console.log(typeof obj);
+// console.log(typeof arr);
+
+
